a6b83063cd66baa584e3efd04406059f0b4c7b64
jp <julia.payer@gmail.com>
2024-06-17 15:22:35 +0200

    first commit

---
+++ src/at/campus02/AlgorithmenDatenstrukturen/SimpleLinkedList/LinkedList.java
@@ -0,0 +1,76 @@
+package at.campus02.AlgorithmenDatenstrukturen.SimpleLinkedList;
+
+public class LinkedList {
+
+    private Node first;
+    private Node last;
+
+    public Node getFirst() {
+        return first;
+    }
+
+    public Node getLast() {
+        return last;
+    }
+
+    //Knoten immer am Ende der Liste anhaengen
+    public void addNodeAtLastPos(String lebensmittel) {
+        Node newNode = new Node(lebensmittel);
+
+        if (first == null) {
+            //code wenn liste leer ist
+            first = newNode;
+            last = newNode;
+        } else {
+            //code wenn Liste nicht mehr leer ist
+            last.setNext(newNode);
+            last = newNode;
+        }
+    }
+
+    public void addNodeAtFirstPos(String lebensmittel) {
+        Node newNode = new Node(lebensmittel);
+
+        if (first == null) {
+            //code wenn liste leer ist
+            first = newNode;
+            last = newNode;
+        } else {
+            newNode.setNext(first);
+            first = newNode;
+        }
+    }
+
+    public void deleteIndexAtPos(int position) {
+        Node current = first;
+        //Suche der korrekten Position
+        //Position 0 bedeutet erster Knoten
+
+        //falls Liste leer ist 0<0
+        if (first == null) {
+            return;
+        } else if (position == 0) {
+            //Wenn Liste nur aus einem Element besteht
+            if (first == last) {
+                last = null;
+            }
+            // zweites wird zum ersten
+            first = current.getNext();
+        } else {
+            //Suchen des Vorgängers
+            //0<(2-1) = 0<1
+            for (int i = 0; i < position - 1; i++) {
+                if (current == last) {
+                    return;
+                }
+                current = current.getNext();
+            }
+            if (current.getNext() == last) {
+                last = current;
+            }
+            current.setNext(current.getNext().getNext());
+            //last gegebenfalls anpassen
+
+        }
+    }
+}
---
+++ src/at/campus02/AlgorithmenDatenstrukturen/SimpleLinkedList/Main.java
@@ -0,0 +1,15 @@
+package at.campus02.AlgorithmenDatenstrukturen.SimpleLinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList linkedList = new LinkedList();
+        linkedList.addNodeAtLastPos("Mango");
+        linkedList.addNodeAtLastPos("Eis");
+        linkedList.addNodeAtLastPos("Joghurt");
+        System.out.println("First: " + linkedList.getFirst().getLebensmittel());
+        System.out.println("Last: " + linkedList.getLast().getLebensmittel());
+        linkedList.deleteIndexAtPos(0);
+        System.out.println("First: " + linkedList.getFirst().getLebensmittel());
+        System.out.println("Last: " + linkedList.getLast().getLebensmittel());
+    }
+}
