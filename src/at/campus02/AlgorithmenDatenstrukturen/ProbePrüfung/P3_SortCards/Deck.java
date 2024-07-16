03ee70962e5ee4f537b217bf1ab6ca6c9d880224
Julia <julia.payer@gmail.com>
2024-07-16 18:23:13 +0200

    probeprüfung 2022

---
+++ src/at/campus02/AlgorithmenDatenstrukturen/ProbePrüfung/P3_SortCards/Deck.java
@@ -0,0 +1,48 @@
+package at.campus02.AlgorithmenDatenstrukturen.ProbePrüfung.P3_SortCards;
+
+public class Deck {
+    private Card[] deck;
+    private int cardCount;
+
+    public Deck(int arraySize) {
+        this.deck = new Card[arraySize];
+        cardCount = 0;
+    }
+
+    public boolean addCard(Card.Color color, int number) {
+        Card newCard = new Card(color, number);
+        if (cardCount == deck.length)
+            return false;
+        else {
+            deck[cardCount] = newCard;
+            cardCount = cardCount + 1;
+            return true;
+        }
+    }
+
+    public Card[] getDeck() {
+        return this.deck;
+    }
+
+    public void sortDeck() {
+        // TODO: Implementierung hinzufuegen
+        for (int i = 1; i < cardCount; i++) {
+            for (int j = i; j >0; j--) {
+                if (deck[j].getColor().compareTo(deck[j-1].getColor()) < 0){
+                    Card temp = deck[j];
+                    deck[j] = deck[j-1];
+                    deck[j-1] = temp;
+                }
+                //wenn die farbe ist gleich, aber die nummer nicht
+                else if ((deck[j].getColor().compareTo(deck[j-1].getColor()) ==0) &&
+                (deck[j].getNumber() < deck[j-1].getNumber())){
+                    Card temp = deck[j];
+                    deck[j] = deck[j-1];
+                    deck[j-1] = temp;
+                } else{
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ src/at/campus02/AlgorithmenDatenstrukturen/ProbePrüfung/P4_GraphBreadthSearch/ListGraph.java
@@ -0,0 +1,80 @@
+package at.campus02.AlgorithmenDatenstrukturen.ProbePrüfung.P4_GraphBreadthSearch;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class ListGraph {
+
+    private ArrayList<Edge>[] graph;
+    private int numVertices;
+    private boolean directed;
+
+    public ListGraph(int numVertices, boolean directed) {
+        graph = new ArrayList[numVertices];
+        for (int i = 0; i < numVertices; i++)
+            graph[i] = new ArrayList<Edge>();
+        this.numVertices = numVertices;
+        this.directed = directed;
+    }
+
+    public int numVertices() {
+        return numVertices;
+    }
+
+    public boolean isDirected() {
+        return directed;
+    }
+
+    public boolean hasEdge(int fromVertex, int toVertex) {
+        Edge edge = findEdge(fromVertex, toVertex);
+        return edge != null;
+    }
+
+    public void addEdge(int fromVertex, int toVertex) {
+        graph[fromVertex].add(new Edge(fromVertex, toVertex));
+        if (!directed) {
+            graph[toVertex].add(new Edge(toVertex, fromVertex));
+        }
+    }
+
+    private Edge findEdge(int fromVertex, int toVertex) {
+        for (Edge edge : graph[fromVertex]) {
+            if (edge.toVertex == toVertex) {
+                return edge;
+            }
+        }
+        return null;
+    }
+
+    public void removeEdge(int fromVertex, int toVertex) {
+        graph[fromVertex].remove(findEdge(fromVertex, toVertex));
+        if (!directed) {
+            graph[toVertex].remove(findEdge(toVertex, fromVertex));
+        }
+    }
+
+    public List<Edge> getEdges(int v) {
+        return graph[v];
+    }
+
+    public List<Integer> performBreadthSearch(int startVertex) {
+        Queue<Integer> queue = new LinkedList<Integer>();
+        ArrayList<Integer> visited = new ArrayList<Integer>();
+        queue.add(startVertex);
+
+        while (!queue.isEmpty()) {
+            int currentVertex = queue.poll();
+            if (visited.contains(currentVertex) == false) {
+                visited.add(currentVertex);
+                //Jede Edge von Array-Position anschauen
+                for (Edge e : graph[currentVertex]) {
+                    queue.add(e.toVertex);
+
+                }
+            }
+        }
+        return visited;
+    }
+}
